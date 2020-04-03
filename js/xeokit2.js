e2c42879b3f6ab2162da19193a031dcdbef6ab74
julianopita <50183295+julianopita@users.noreply.github.com>
2020-04-03 15:37:04 -0300

    teste xeokit2.js e habilitação forum

--- js/xeokit2.js
+++ js/xeokit2.js
@@ -1,185 +1,225 @@
-    //------------------------------------------------------------------------------------------------------------------
-    // Import the modules we need for this example
-    //------------------------------------------------------------------------------------------------------------------
 
-    import {Viewer} from "./../xeokit/src/viewer/Viewer.js";
-    import BimServerClient from "../xeokit/src/plugins/BIMServerLoaderPlugin/BIMServerClient/bimserverclient.js";
-    import {BIMServerLoaderPlugin} from "../xeokit/src/plugins/BIMServerLoaderPlugin/BIMServerLoaderPlugin.js";
-    import {NavCubePlugin} from "../xeokit/src/plugins/NavCubePlugin/NavCubePlugin.js";
-    import {TreeViewPlugin} from "../xeokit/src/plugins/TreeViewPlugin/TreeViewPlugin.js";
 
-    //------------------------------------------------------------------------------------------------------------------
-    // BIMServer parameters for this example
-    //------------------------------------------------------------------------------------------------------------------
 
-    const bimServerAddress = "http://www.nomads.usp.br:8080/bimserver/";
+import {Viewer} from "../xeokit/src/viewer/Viewer.js";
+import {BIMServerLoaderPlugin} from "../xeokit/src/plugins/BIMServerLoaderPlugin/BIMServerLoaderPlugin.js";
+import BimServerClient from "../xeokit/src/plugins/BIMServerLoaderPlugin/BIMServerClient/bimserverclient.js";
+// import {ReadableGeometry} from "../xeokit/src/viewer/scene/geometry/ReadableGeometry.js";
+import {AnnotationsPlugin} from "../xeokit/src/plugins/AnnotationsPlugin/AnnotationsPlugin.js";
+//import {DistanceMeasurementsPlugin} from "../bimnomads2/xeokit/src/plugins/DistanceMeasurementsPlugin/DistanceMeasurementsPlugin.js";
+// import {StoreyViewsPlugin} from "../xeokit/src/plugins/StoreyViewsPlugin/StoreyViewsPlugin.js";
+// import {XKTLoaderPlugin} from "../src/plugins/XKTLoaderPlugin/XKTLoaderPlugin.js";
 
-    const username = "platnomads@gmail.com";
-    const password = "@bimserver";
 
-    const poid = 196609;
+const bimServerAddress = "http://www.nomads.usp.br:8080/bimserver/";
+const username = "platnomads@gmail.com";
+const password = "@bimserver";
+const poid = 196609;
+const roid = 131075;
 
-    //------------------------------------------------------------------------------------------------------------------
-    // Create a Viewer,
-    // position the camera and
-    // tweak the default xraying and highlighting materials
-    //------------------------------------------------------------------------------------------------------------------
 
-    const viewer = new Viewer({
-        canvasId: "canvas_aux1",
-        transparent: true
-    });
+// Create a Viewer
+const viewer = new Viewer({
+    canvasId: "canvas_aux1"
+});
+// Create a BIMServerClient
+const bimServerClient = new BimServerClient(bimServerAddress);
 
-    viewer.camera.eye = [-3.06, 8.19, 11.253];
-    viewer.camera.look = [6.22, 5.84, -2.10];
-    viewer.camera.up = [0.08, 0.98, -0.11];
+// Add a BIMServerLoaderPlugin
+const bimServerLoader = new BIMServerLoaderPlugin(viewer, {
+    bimServerClient: bimServerClient
+});
+// Create a Annotation plugin
+// const annotations = new AnnotationsPlugin(viewer, {
 
-    viewer.scene.xrayMaterial.fillAlpha = 0.1;
-    viewer.scene.xrayMaterial.fillColor = [0, 0, 0];
-    viewer.scene.xrayMaterial.edgeAlpha = 0.4;
-    viewer.scene.xrayMaterial.edgeColor = [0, 0, 0];
+//     markerHTML: "<div class='annotation-marker' style='background-color: {{markerBGColor}};'>{{glyph}}</div>",
+//     labelHTML: "<div class='annotation-label' style='background-color: {{labelBGColor}};'><div class='annotation-title'>{{title}}</div><div class='annotation-desc'>{{description}}</div></div>",
 
-    viewer.scene.highlightMaterial.fillAlpha = 0.3;
-    viewer.scene.highlightMaterial.edgeColor = [1, 1, 0];
+//     values: {
+//         markerBGColor: "red",
+//         glyph: "X",
+//         title: "Untitled",
+//         description: "No description"
+//     }
+// });
 
-    //------------------------------------------------------------------------------------------------------------------
-    // Create an IFC structure tree view
-    //------------------------------------------------------------------------------------------------------------------
 
-    const treeView = new TreeViewPlugin(viewer, {
-        containerElement: document.getElementById("treeViewContainer"),
-        autoExpandDepth: 3 // Initially expand three nodes deep
-    });
-
-    //------------------------------------------------------------------------------------------------------------------
-    // Create a NavCube
-    //------------------------------------------------------------------------------------------------------------------
-
-    new NavCubePlugin(viewer, {
-        canvasId: "myNavCubeCanvas",
-        visible: true, // Default
-        size: 250, // NavCube canvas size in pixels (default is 200)
-        alignment: "bottomRight", // "bottomLeft" (default) | "topLeft" | "topRight" | "bottomRight"
-        bottomMargin: 100,
-        rightMargin: 10
-    });
-
-    //------------------------------------------------------------------------------------------------------------------
-    // Mouse over objects to highlight them
-    //------------------------------------------------------------------------------------------------------------------
-
-    var lastEntity = null;
-
-    viewer.scene.input.on("mousemove", function (coords) {
-        var hit = viewer.scene.pick({
-            canvasPos: coords
-        });
-        if (hit) {
-            if (!lastEntity || hit.entity.id !== lastEntity.id) {
-                if (lastEntity) {
-                    lastEntity.highlighted = false;
-                }
-                lastEntity = hit.entity;
-                hit.entity.highlighted = true;
-            }
-        } else {
-            if (lastEntity) {
-                lastEntity.highlighted = false;
-                lastEntity = null;
-            }
-        }
-    });
-
-    //------------------------------------------------------------------------------------------------------------------
-    // Click objects to get their IFC info in the JS console
-    //------------------------------------------------------------------------------------------------------------------
-
-    viewer.scene.input.on("mouseclicked", function (coords) {
-
-        var hit = viewer.scene.pick({
-            canvasPos: coords
-        });
-
-        if (hit) {
-            var entity = hit.entity;
-            var metaObject = viewer.metaScene.metaObjects[entity.id];
-            if (metaObject) {
-                console.log(JSON.stringify(metaObject.getJSON(), null, "\t"));
-            } else {
-                const parent = entity.parent;
-                if (parent) {
-                    metaObject = viewer.metaScene.metaObjects[parent.id];
-                    if (metaObject) {
-                        console.log(JSON.stringify(metaObject.getJSON(), null, "\t"));
-                    }
-                }
-            }
-        }
-    });
-
-    //------------------------------------------------------------------------------------------------------------------
-    // Create a BIMServerClient
-    //------------------------------------------------------------------------------------------------------------------
-
-    const bimServerClient = new BimServerClient(bimServerAddress);
-
-    //------------------------------------------------------------------------------------------------------------------
-    // Add a BIMServerLoaderPlugin to the Viewer
-    //------------------------------------------------------------------------------------------------------------------
-
-    const bimServerLoader = new BIMServerLoaderPlugin(viewer, {
-        bimServerClient: bimServerClient
-    });
-
-    //------------------------------------------------------------------------------------------------------------------
-    // Initialize the BIMServerClient
-    //------------------------------------------------------------------------------------------------------------------
-
-    bimServerClient.init(() => {
-
-        //--------------------------------------------------------------------------------------------------------------
-        // Login to BIMServer
-        //--------------------------------------------------------------------------------------------------------------
-
-        bimServerClient.login(username, password, () => {
-
-            //----------------------------------------------------------------------------------------------------------
-            // Get project info via the BIMServer RPC ServiceInterface
-            //----------------------------------------------------------------------------------------------------------
-
-            bimServerClient.call("ServiceInterface", "getProjectByPoid", {
-                poid: poid
+// Initialize the BIMServerClient
+bimServerClient.init(() => {
+    // Login to the BIMServerClient
+    bimServerClient.login(username, password,() => {
+        // Get information on our project
+        bimServerClient.call("ServiceInterface", "getProjectByPoid", {
+            poid: poid 
             }, (project) => {
 
-                //------------------------------------------------------------------------------------------------------
-                // Load the latest revision of the project using the BIMServerLoaderPlugin
-                // The BIMServerLoaderPlugin used the BIMServerClient to load the model and IFC metadata
-                //------------------------------------------------------------------------------------------------------
-
-                // Load the latest revision of the project
-                // Use whatever IFC schema that's for
+                // Load the latest revision of the project.
+                // Use whatever IFC schema the model uses.
 
                 const roid = project.lastRevisionId;
+                    console.log(roid);
                 const schema = project.schema;
 
+                // Load our model from BIMServer
                 const model = bimServerLoader.load({ // Returns a Node representing the model
-
-                    id: "myModel",                  // Assigned to Node#id, and Node#isModel will also be set true
-
+                    id: "myModel2",                   // Assigned to Node#id, and Node#isModel will also be set true
                     poid: poid,
                     roid: roid,
-                    schema: schema,
-
-                    scale: [0.001, 0.001, 0.001],   // Assigned to Node#scale, since model boundary exceeds view volume
-                    rotation: [-90, 0, 0],          // Assigned to Node#rotation, since model "up" is +Z
-                    edges: true                    // Assigned to Node#edges, to emphasis edges
+                    schema: schema,                 // Load the schema that's available for our project
+                    scale: [1, 1, 1],   // Scale the model
+                    rotation: [-90, 0, 0],          // Rotate the model
+                    edges: true                     // Emphasise edges
                 });
 
-            }, function (response) {
-                console.log("BIMServer getProjectByPoid failed: " + response.message);
-            });
+                // Fit camera to model when loaded
+                model.on("loaded", function() {
+                    viewer.cameraFlight.jumpTo(model);
+                });
 
-        }, function (response) {
-            console.log("BIMServer login failed: " + response.message);
-        });
+                model.on("error", function (errMsg) {
+                    console.error("Loading failed: " + errMsg);
+                });
+            });
+    },(error) => {
+        console.log("Error logging in: " + error.message);
     });
+});
+
+
+
+
+const annotations = new AnnotationsPlugin(viewer, {
+
+    markerHTML: "<div class='annotation-marker' style='background-color: {{markerBGColor}};'>{{glyph}}</div>",
+    labelHTML: "<div class='annotation-label' style='background-color: {{labelBGColor}};'><div class='annotation-title'>{{title}}</div><div class='annotation-desc'>{{description}}</div></div>",
+
+    values: {
+        markerBGColor: "blue",
+        glyph: "X",
+        title: "Untitled",
+        description: "No description"
+    }
+});
+
+// annotations.on("markerClicked", (annotation) => {
+//     annotation.labelShown = !annotation.labelShown;
+// });
+
+//------------------------------------------------------------------------------------------------------------------
+// Highlights selected entities
+//------------------------------------------------------------------------------------------------------------------
+var i = 1;
+var lastEntity = null;
+var flag = -1;
+var annotation = null;
+
+viewer.scene.input.on("mouseclicked",async function (coords) {
+    // coords[0] = coords[0] - 100;
+    // coords[1] = coords[1] - 100;
+    var hit = viewer.scene.pick({
+        canvasPos: coords,
+        pickSurface : true
+    });
+    
+    if (hit) {
+            if(flag == 1 && annotation != null){
+                annotation.destroy();
+                flag = -1;
+            }
+            calls(hit);
+            if (!lastEntity || hit.entity.id !== lastEntity.id) {
+
+            if (lastEntity) {
+                lastEntity.highlighted = false;
+            }
+
+            lastEntity = hit.entity;
+            hit.entity.highlighted = true;
+            if (hit) {
+
+                annotation = await annotations.createAnnotation({
+                    id: "myAnnotation" + i,
+                    pickResult: hit, // <<------- initializes worldPos and entity from PickResult
+                    occludable: true,       // Optional, default is true
+                    markerShown: true,      // Optional, default is true
+                    labelShown: true,       // Optional, default is true
+                    values: {               // HTML template values
+                        glyph: "i",
+                        title: sessionStorage.getItem("IfcOidName"),
+                        description: "Área: " + sessionStorage.getItem("IfcOidArea")
+                }        
+                });
+                flag = 1;
+                i++;
+            }
+            // document.getElementsByClassName('annotation-marker')[0].style.setProperty('visibility','visible');
+        }
+    } else {
+
+        if (lastEntity) {
+            lastEntity.highlighted = false;
+            lastEntity = null;
+        }
+    }
+    
+});
+
+
+
+//chamadas ao servidor
+async function calls(hit){
+    await bimServerClient.call("LowLevelInterface", "getDataObjectByGuid",
+    {roid: roid , guid: hit.entity.id},
+    function(IfcBuildingData){
+        //obterm o Oid do objeto e o salva em uma sessao local
+        sessionStorage.setItem("IfcBuildingOid", IfcBuildingData.oid);
+        console.log(IfcBuildingData.oid);
+    });
+    //get Area from Oid
+    await bimServerClient.call("ServiceInterface", "getArea",  {roid: roid , oid: sessionStorage.getItem('IfcBuildingOid')}, 
+        function(area){
+        //obterm o Oid do site e o salva em uma sessao local
+            sessionStorage.setItem("IfcOidArea", area);
+            console.log(area);
+        //closing Area
+        });
+    await bimServerClient.call("LowLevelInterface", "getDataObjectByOid",  {roid: roid , oid: sessionStorage.getItem('IfcBuildingOid')}, 
+        function(data){
+        // obterm o Oid do site e o salva em uma sessao local
+            sessionStorage.setItem("IfcOidName", data.name);
+            console.log(data.name);
+       })
+    
+}
+
+
+
+// Testing annotations
+
+
+
+viewer.camera.eye = [-3.93, 2.85, 27.01];
+viewer.camera.look = [4.40, 3.72, 8.89];
+viewer.camera.up = [-0.01, 0.99, 0.039];
+
+
+
+//------------------------------------------------------------------------------------------------------------------
+// Use the AnnotationsPlugin to create an annotation wherever we click on an object
+//------------------------------------------------------------------------------------------------------------------
+
+
+
+// viewer.scene.input.on("mouseclicked", (coords) => {
+//     console.log(coords);
+//     coords[1] = coords[1] - 10;
+//     console.log(coords);
+//     var pickResult = viewer.scene.pick({
+//         canvasPos: coords,
+//         pickSurface: true  // <<------ This causes picking to find the intersection point on the entity
+//     });
+
+ 
+// });
+
+window.viewer = viewer;

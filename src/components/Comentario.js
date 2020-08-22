2f22966f6e7e4858e2af84686d949ef99aaf03df
victorhugol <victorsilva698@usp.br>
2020-08-22 16:04:47 -0300

    transformação de App em um componente

--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -6,18 +6,26 @@ const Comentario = {
     name : 'comentario',
     template : `
         <div id="comentario">
-            <ul>
-                <li v-for="(comentario,i) in coment">
-                   {{i}} {{comentario.name}} 
-                </li>
-            </ul>
-
+            <div v-for="(comentario,i) in dadoTratado" :key="i">
+                <div v-for="(j,index) in comentario" :key="index">
+                    <div v-if="index == 0" id="mainCom">
+                        <div class="mainName">{{j.name}} </div>
+                        <div class="mainBody">{{j.body}} </div>
+                    </div >
+                        
+                    <div v-else id="answer">
+                        <div class="mainName">{{j.name}} </div>
+                        <div class="mainBody"> {{j.body}}</div>
+                    </div>
+                </div>
+            </div>
             <link rel="stylesheet" href="./src/styles/comentario.css">
         </div>
     `,
     data(){
         return {
-            coment : ""
+            coment : "",
+            dadoTratado : []
         }
     },
     methods : {
@@ -29,13 +37,44 @@ const Comentario = {
                 muda = params;
             })
             this.coment = muda;
+        },
+        //trata os dados vindo do Get
+        manipulaGet : function(getResponse) {
+            var postId = 1;
+            var listaComentarios = [];
+            var lenComentOne = 0;
+            
+            //adiciona apena o primeiro comentario e resposta ao 
+            // dadoTratado
+            for (let index = 0; index < getResponse.length; index++) {
+                if(postId == getResponse[index].postId){
+                    listaComentarios.push(getResponse[index]);
+                }else{
+                    this.dadoTratado.push(listaComentarios);
+                    break;
+                }
+            }
+
+            console.log(this.dadoTratado);
+            lenComentOne = listaComentarios.length;
+            listaComentarios = [];
+            for (lenComentOne; lenComentOne < getResponse.length; lenComentOne++){
+                if(getResponse[lenComentOne].postId != postId){
+                    this.dadoTratado.push(listaComentarios);
+                    listaComentarios = [];
+                    postId = getResponse[lenComentOne].postId;
+                }
+                listaComentarios.push(getResponse[lenComentOne]);
+            }
+            this.dadoTratado.push(listaComentarios);
+            console.log(this.dadoTratado);
         }
 
 
-
     },
-    mounted(){
-        this.get();
+    mounted: async function(){
+        // await this.get();
+        // this.manipulaGet(this.coment);
 
 
     }
--- src/index.js
+++ src/index.js
@@ -1,61 +1,6 @@
-import Vue from './lib/vue.js';
-import NavBar from './components/NabBar.js';
-import CanvasComponent from './components/Canvas.js';
-import clientNomads from './xeokit.js';
-import Forum from './components/Forum.js';
-const bimServerAddress = "http://plat-nomads.ddns.net:8080/bimserver/";
-const poid = 131073;
-const roid = 65539;
+import App from './App.js';
 
 
-
-const App = {
-    name : 'App',
-    
-    
-    components : {
-        NavBar,
-        CanvasComponent,
-        Forum
-    },
-
-
-    template : 
-    `<div>
-
-        <NavBar/>
-
-        <div id="layoutMain">
-            <link rel="stylesheet" href="./src/styles/principal.css">
-
-
-                <div id="leftSide">
-                    <div class="generalInfo" > </div>
-                    
-                    <div class="canvasDiv">
-                        <CanvasComponent/>
-                    </div>
-                    
-                    <div class="infoSpace"></div>
-        
-                </div>
-                
-                    <Forum/>
-        </div>
-    </div>
-        `,
-
-    data(){
-        return{
-        }
-    },
-
-    mounted(){
-        // let a = new clientNomads(bimServerAddress,poid,"c1", roid);
-        
-    }
-
-}
 new Vue({
     el: '#app',
     components : {

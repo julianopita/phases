05cafd9e2544f58fc244d7ee511d4e3607016f36
julianopita <50183295+julianopita@users.noreply.github.com>
2020-10-15 17:44:48 -0300

    Mais formatação e ocultar respostas nao funcionando

--- src/components/plataforma/forum/Comentario.js
+++ src/components/plataforma/forum/Comentario.js
@@ -6,18 +6,23 @@ const Comentario = {
         <div class="comentario">
 
             <div class="divComentario">                
-                <p class="dados">{{item.userName}} | 15 out 2020 | 4 Comentários</p>
-                <p class="texto">{{item.comentario}}</p>
+                <p class="dados">{{item.userName}} | 15 out 2020 | 4 Comentários |</p>
+                <p class="texto">{{item.comentario}} frase mais comprida para ver se o que ele faz</p>
                 <p><a class="interesse">Morador</a><a class="tag">Implantação</a></p>
             </div>
-
-            <ul class="respostas">
-                <li v-for="(resposta,i) in item.resposta">                    
-                    <p>{{resposta.userName}}</p>
-                    <p>{{resposta.comentario}}</p>
-                    <p>{{resposta.interesse}}</p>
+            
+            <div class="respostas">
+            <button class="hide" @click="showDiscussion">Mostrar discussão</button>  
+            <ul class="respostas">            
+                <li class="target" v-for="(resposta,i) in item.resposta">   
+                               
+                    <p class="dados">{{resposta.userName}} | 15 out 2020</p>
+                    <p>{{resposta.comentario}} também comprida será que ele vai quebrar?</p>
+                    
+                    <p><a class="interesse">Morador</a></p>
                 </li>
             </ul>
+            </div>
             <div class="nova-resposta">
                 <textarea v-model="comentario" :cols=cols :rows=rows placeholder="nova conversa">
                 </textarea>
@@ -44,6 +49,20 @@ const Comentario = {
                 interesse : "interesse"
             });
             this.comentario = '';
+        },
+        
+        showDiscussion : function() {
+            document.addEventListener("click",function(e) {
+            if (e.target.className.match(/\bhide\b/)) {
+                e.preventDefault();
+                // find the related target span
+                console.log(e);
+                var target = e.target.parentNode.querySelector("respostas");
+                console.log(target);
+                // update its visibility
+                target.style.display = target.style.display === "none" ? "block" : "none";
+                }    
+            })
         }
     },
     components : {
